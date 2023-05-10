c116c7d726784f361839e5a8f521523d7027371a
Sergio Zapata <56563008+sergiozap13@users.noreply.github.com>
2023-05-10 12:48:32 +0200

    Archivos de configuración

--- build/Debug/GNU-Linux/src/main.o.d
+++ build/Debug/GNU-Linux/src/main.o.d
@@ -1,1 +1,5 @@
-build/Debug/GNU-Linux/src/main.o: src/main.cpp
+build/Debug/GNU-Linux/src/main.o: src/main.cpp include/Language.h \
+ include/BigramFreq.h include/Bigram.h
+include/Language.h:
+include/BigramFreq.h:
+include/Bigram.h:
